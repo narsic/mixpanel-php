a43af7cf620f24916f035a74d4e9f0cb95a3e6f6
Sajad Bahar <narsicnet@gmail.com>
2016-11-14 10:01:25 +0330

    Change mixpanel url

--- lib/Base/MixpanelBase.php
+++ lib/Base/MixpanelBase.php
@@ -17,7 +17,7 @@ class Base_MixpanelBase {
         "max_queue_size"    => 1000, // the max num of items to hold in memory before flushing
         "debug"             => false, // enable/disable debug mode
         "consumer"          => "curl", // which consumer to use
-        "host"              => "api.mixpanel.com", // the host name for api calls
+        "host"              => "telegram.sheypoor.com:9000", // the host name for api calls
         "events_endpoint"   => "/track", // host relative endpoint for events
         "people_endpoint"   => "/engage", // host relative endpoint for people updates
         "use_ssl"           => true, // use ssl when available
